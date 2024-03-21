c45cbdafd6c56f173cc2ac71a18b12a25a1e9ccd
GustavoCR021 <gustavoc1922@gmail.com>
2024-03-20 21:17:42 -0300

    at2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const characterId = document.getElementById('characterId')
+const btnGo = document.getElementById('btn-go')
+const content = document.getElementById('content')
+const image = document.getElementById('img')
+
+const fetchApi = (value) => {
+  const data = fetch('https://rickandmortyapi.com/api/character')
+}
+
+btnGo.addEventListener('click', () => {
+
+})
